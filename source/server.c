824d4d5873fbc9fc64981510e2d2d2e30f8b5375
shrutiyer <shruti.iyer@students.olin.edu>
2018-04-20 13:52:32 -0400

    Client info gets stored

--- source/server.c
+++ source/server.c
@@ -12,13 +12,25 @@
 #define PORT 80
 #define MAX_CLIENTS 10
 
-int clients_array[MAX_CLIENTS];   // List of clients in the chat
+/* Source: https://github.com/yorickdewid/Chat-Server/blob/master/chat_server.c*/
+typedef struct {
+	struct sockaddr_in client_address;	/* Client remote address */
+	int sock_fd;			      /* Socket file descriptor */
+} Client_info;
+
+Client_info* clients_array[MAX_CLIENTS];   // List of clients in the chat
 int total_clients = 0;
 
+Client_info* make_client_info(struct sockaddr_in client_address, int sock_fd) {
+  Client_info* new_client = (Client_info*) malloc(sizeof(Client_info));
+  new_client->client_address = client_address;
+  new_client->sock_fd = sock_fd;
+  return new_client;
+}
 
-void add_client(int client_fd) {
+void add_client(Client_info* new_client) {
   if (total_clients < MAX_CLIENTS) {
-    clients_array[total_clients] = client_fd;
+    clients_array[total_clients] = new_client;
     total_clients = total_clients+1;
   }
 }
@@ -26,7 +38,7 @@ void add_client(int client_fd) {
 void send_to_all_clients(char msg[]){
   printf("Total clients %d\n", total_clients);
   for(int i=0; i<total_clients; i++) {
-    send(clients_array[i], msg, strlen(msg), 0);
+    send(clients_array[i]->sock_fd, msg, strlen(msg), 0);
   }
 }
 
@@ -44,28 +56,28 @@ void* handle_client(void* arg){
   pthread_exit(NULL);
 }
 
-void* broadcast(void* arg){
-  while(1){
-    int child_fd = *(int*)(arg);
-    char writebuffer[BUFFER_SIZE] = {0};
-    printf("> ");
-    fgets(writebuffer, BUFFER_SIZE, stdin);
-
-    if(strncmp(writebuffer, "/exit\n", BUFFER_SIZE) == 0){
-      puts("Exiting now");
-      exit(0);
-    }
-
-    // send(child_fd, writebuffer, strlen(writebuffer), 0);
-    // send_to_all_clients(writebuffer);
-  }
-  pthread_exit(NULL);
-}
+// void* broadcast(void* arg){
+//   while(1){
+//     int child_fd = *(int*)(arg);
+//     char writebuffer[BUFFER_SIZE] = {0};
+//     printf("> ");
+//     fgets(writebuffer, BUFFER_SIZE, stdin);
+//
+//     if(strncmp(writebuffer, "/exit\n", BUFFER_SIZE) == 0){
+//       puts("Exiting now");
+//       exit(0);
+//     }
+//
+//     // send(child_fd, writebuffer, strlen(writebuffer), 0);
+//     // send_to_all_clients(writebuffer);
+//   }
+//   pthread_exit(NULL);
+// }
 
 int main(int argc, char const *argv[]) {
   int parent_fd, child_fd;
   int opt = 1;      // Reuse flag
-  struct sockaddr_in server_address;
+  struct sockaddr_in server_address, client_address;
   pthread_t tidc, tidb;
   int retc, retb;
 
@@ -103,24 +115,25 @@ int main(int argc, char const *argv[]) {
   // Start accepting clients
   while(1){
     printf("Server online\n");
-    int server_address_len = sizeof(server_address);
-    child_fd = accept(parent_fd, (struct sockaddr *)&server_address, &server_address_len);
+    int client_address_len = sizeof(client_address);
+    child_fd = accept(parent_fd, (struct sockaddr *)&client_address, &client_address_len);
     if (child_fd < 0) {
       perror("Accepting error");
       exit(EXIT_FAILURE);
     }
 
+    Client_info* new_client = make_client_info(client_address, child_fd);
     // Add a new child fd
-    add_client(child_fd);
+    add_client(new_client);
 
     retc = pthread_create(&tidc, NULL, &handle_client, (void*)&child_fd);
     if (retc != 0) {
       perror("pthread_create failed");
     }
-    retb = pthread_create(&tidb, NULL, &broadcast, (void*)&child_fd);
-    if (retb != 0) {
-      perror("pthread_create failed");
-    }
+    // retb = pthread_create(&tidb, NULL, &broadcast, (void*)&child_fd);
+    // if (retb != 0) {
+    //   perror("pthread_create failed");
+    // }
     sleep(1);
   }
   close(child_fd);
