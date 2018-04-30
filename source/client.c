dd46e1d300769457b00558b155baae19add8f0ab
shrutiyer <shruti.iyer@students.olin.edu>
2018-04-30 17:03:11 -0400

    WIP: Two windows with characters printed to the user input window

--- source/client.c
+++ source/client.c
@@ -8,9 +8,13 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <pthread.h>
+#include <ncurses.h>
 
 #define PORT 80
 #define BUFFER_SIZE 1024
+#define KEY_ENTER 13
+
+WINDOW *user_input, *chat_window;
 
 void* handle_server(void* arg) {
   int sock_fd = *(int*)arg;
@@ -18,17 +22,43 @@ void* handle_server(void* arg) {
   int read_val;
 
   while ((read_val = read(sock_fd, buffer, BUFFER_SIZE)) > 0) {
-
-    printf("%s\n", buffer);
+    // printf("%s\n", buffer);
+    mvwprintw(chat_window, 1, 1,"%s\n", buffer);
+    wrefresh(chat_window);
     memset(buffer, 0, BUFFER_SIZE);
   }
   pthread_exit(NULL);
+}
+
+static void init_ncurses(void) {
+	initscr();
+	cbreak();
+  clear();
+	noecho();
+
+  int x_max, y_max;
+  getmaxyx(stdscr, y_max, x_max);
+
+  user_input = newwin(3, x_max-12, y_max-5, 5);
+  box(user_input, 0, 0);
+  wrefresh(user_input);
+  keypad(user_input, true);
+
+  chat_window = newwin(y_max-8, x_max-12, 3, 5);
+  box(chat_window, 0, 0);
+  wrefresh(chat_window);
+}
+
+void end_ncurses(void) {
+  getch();
+  endwin();
 }
 
 int main(int argc, char const *argv[]) {
   int sock_fd;
   struct sockaddr_in server_address;
 
+  init_ncurses();
   sock_fd = socket(AF_INET, SOCK_STREAM, 0);
   if (sock_fd < 0) {
     perror("Socket creation error");
@@ -37,7 +67,7 @@ int main(int argc, char const *argv[]) {
 
   memset(&server_address, 0, sizeof(server_address));
   server_address.sin_family = AF_INET;
-  server_address.sin_addr.s_addr = inet_addr("167.99.229.132");
+  //server_address.sin_addr.s_addr = inet_addr("167.99.229.132");
   server_address.sin_port = htons(PORT);
 
   int connect_val = connect(sock_fd, (const struct sockaddr *)&server_address, sizeof(server_address));
@@ -50,10 +80,21 @@ int main(int argc, char const *argv[]) {
   pthread_create(&tid, NULL, &handle_server, (void*)&sock_fd);
   while(1) {
     char msg_from_client[BUFFER_SIZE] = {0};
-    printf("> ");
-    fgets(msg_from_client, BUFFER_SIZE, stdin);
+    mvwprintw(user_input, 1, 1,">");
+    wrefresh(user_input);
+    // fgets(msg_from_client, BUFFER_SIZE, stdin);
+    int ch = 0, x = 1, y = 1;
+		while(ch != KEY_ENTER)
+		{
+      ch = wgetch(user_input);
+			mvwprintw(user_input, y, x, "%c", ch);
+			wrefresh(user_input);
+			++x;
+		}
+
     if(strncmp(msg_from_client, "/exit\n", BUFFER_SIZE) == 0){
       puts("exiting....");
+      end_ncurses();
       exit(0);
     }
     int send_val = send(sock_fd, msg_from_client, strlen(msg_from_client), 0);
